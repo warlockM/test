4445d1c9eb1c6b89a320f13f75f036588951db2b
mayank <mnkraj529@gmail.com>
2022-12-04 01:42:55 +0530

    Logo fixed

--- design/pages/components/navbar.js
+++ design/pages/components/navbar.js
@@ -1,12 +1,23 @@
 import Link from 'next/link'
+import Image from 'next/image';
 export default function Navbar() {
     return (
-    <div class="flex place-content-center space-x-8 bg-slate-100 p-5 shadow-lg rounded-md font-bold text-[#1E3888] w-screen">
-        <div><img src = "../images/logo.png" alt = "logo" class = "w-[200px]"></img></div>
-          <div><Link href = "/">Home</Link></div>
-          <div><Link href = "/about">About</Link></div>
-          <div><Link href = "/service">Services</Link></div>
-          <div><Link href = "/treatment">Treatment</Link></div>
-    </div>
-    )
+      <div class="flex place-content-center space-x-8 bg-slate-100 p-0 shadow-lg rounded-md font-bold text-[#1E3888] w-screen">
+        <div class="p-0">
+          <Image src="/logo.png" alt="logo" width={200} height={100} />
+        </div>
+        <div class="mt-10">
+          <Link href="/">Home</Link>
+        </div>
+        <div class="mt-10">
+          <Link href="/about">About</Link>
+        </div>
+        <div class="mt-10">
+          <Link href="/service">Services</Link>
+        </div>
+        <div class="mt-10">
+          <Link href="/treatment">Treatment</Link>
+        </div>
+      </div>
+    );
 }
--- design/pages/index.js
+++ design/pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import Navbar from './components/navbar';
 import Footer from './components/footer';
-import ConnectForm from './components/connectForm';
 import Link from 'next/link';
+import Image from 'next/image';
+
 
 export default function Home() {
 
@@ -14,50 +15,66 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <h1 className="text-5xl text-[#1E3888] font-bold  m-20 mt-20 text-center 
-      letter-spacing: -0.05em underline decoration-wavy decoration-slate-100">
+      <h1
+        className="text-5xl text-[#1E3888] font-bold m-20 text-center 
+      letter-spacing: -0.05em underline decoration-wavy decoration-slate-100"
+      >
         Empowering Minds, Transforming Lives
       </h1>
-      <div class= " bg-slate-50 shadow-sm p-11 shadow-lg">
+      <div class=" bg-slate-100 p-11">
         <h3 class="text-slate-500 text-center font-bold">
-          Heal100 is an Initiative that cater to the allround development of young
-          children<span class="block"></span> and address their mental wellness
-          needs
+          Heal100 is an Initiative that cater to the allround development of
+          young children<span class="block"></span> and address their mental
+          wellness needs
         </h3>
 
-      <div class="m-10 flex place-content-center space-x-6 text-xl font-bold text-[#1E3888]">
-        <div>Counseling</div>
-        <div>▪</div>
-        <div>Therapy</div>
-        <div>▪</div>
-        <div>Wellness</div>
-        <div>▪</div>
-        <div> Lifeskill</div>
-      </div>
+        <div class="m-10 flex place-content-center space-x-6 text-xl font-bold text-[#1E3888]">
+          <div>Counseling</div>
+          <div>▪</div>
+          <div>Therapy</div>
+          <div>▪</div>
+          <div>Wellness</div>
+          <div>▪</div>
+          <div> Lifeskill</div>
+        </div>
 
-      <div class="flex place-content-center space-x-3">
-      <Link href="/connect">
-          <button class="bg-slate-200 text-slate-700 text-sm p-4 shadow-md shadow-slate-500 font-bold tracking-wide
-           border-black border-1 pr-7 pl-7 rounded-lg">
-             Learn more
-          </button>
-        </Link>
-        <Link href="/connect">
-          <button class="bg-blue-900 text-white text-sm p-4 shadow-md shadow-slate-500 font-bold 
-          tracking-wide pr-7 pl-7 rounded-lg">
-            Connect with us
-          </button>
-        </Link>
+        <div class="flex place-content-center space-x-3">
+          <Link href="/connect">
+            <button
+              class="bg-slate-200 text-slate-700 text-sm p-4 shadow-md shadow-slate-500 font-bold tracking-wide
+           border-black border-1 pr-7 pl-7 rounded-lg"
+            >
+              Learn more
+            </button>
+          </Link>
+          <Link href="/connect">
+            <button
+              class="bg-blue-900 text-white text-sm p-4 shadow-md shadow-slate-500 font-bold 
+          tracking-wide pr-7 pl-7 rounded-lg"
+            >
+              Connect with us
+            </button>
+          </Link>
         </div>
       </div>
-      
-      <div class = "flex place-content-center">
-        <p class="text-3xl text-[#1E3888] font-bold  m-20 mt-20 text-center letter-spacing: -0.05em underline decoration-wavy decoration-slate-100 offset-8">
-          Stress is not going anywhere, <span class="block"></span>but we can be
-          better prepared!
+
+      <p class="place-content-center text-3xl text-[#1E3888] font-bold mb-8 mx-auto mt-8 text-center letter-spacing: -0.05em underline decoration-wavy decoration-slate-100 offset-8">
+        Stress is not going anywhere, <span class="block"></span>but we can be
+        better prepared!
+      </p>
+      <div class="flex place-content-center space-x-8 m-10 p-7">
+        <p class="text-xl text-slate-500 font-bold">
+          A stressed student is 73% likely to underperform under stress.
         </p>
-      </div >
-      
+        <Image
+          src="/School.jpg"
+          alt="connect"
+          class="w-1/4 place-content-center"
+          width={500}
+          height={500}
+        />
+        <div />
+      </div>
     </div>
   );
 }
